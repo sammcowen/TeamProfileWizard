8cd914da3cc605862a7a8b8b84dd676670d691bd
HBK <sammcowen@gmail.com>
2022-02-25 14:57:56 -0800

    created menu to ask for engineer,intern or finish building team

--- index.js
+++ index.js
@@ -1,69 +1,98 @@
 // Required packages 
 const inquirer = require('inquirer');
-
 const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
+const teamArr = [];
 
 // questions to create team 
-const managerQuestions = [
-    {
-        type: 'input',
-        name: 'managerName',
-        message: 'Enter MANAGER name:',
-        validate: managerNameInput => {
-            if (managerNameInput) {
-                return true;
-            } else {
-                console.log('You MUST to enter a name to continue!');
-                return false;
+const managerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter MANAGER name:',
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    return true;
+                } else {
+                    console.log('You MUST to enter a name to continue!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'employeeId',
-        message: 'Enter employee id for MANAGER:',
-        validate: employeeIdInput => {
-            if (employeeIdInput) {
-                return true;
-            } else {
-                console.log('You MUST enter a employee ID to continue!');
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee id for MANAGER:',
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter a employee ID to continue!');
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'emailAddress',
-        message: 'Enter MANAGER email address:',
-        validate: emailAddressInput => {
-            if (emailAddressInput) {
-                return true;
-            } else {
-                console.log('You MUST enter an email address to continue!');
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter MANAGER email address:',
+            validate: emailAddressInput => {
+                if (emailAddressInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an email address to continue!');
+                }
             }
-        }
-    },
-    {
-        type: "input",
-        name: "officeNum",
-        message: "Enter MANAGER office number:",
-        validate: officeNumInput => {
-            if (officeNumInput) {
-                return true;
-            } else {
-                console.log('You MUST enter an office number to continue!');
+        },
+        {
+            type: "input",
+            name: "officeNum",
+            message: "Enter MANAGER office number:",
+            validate: officeNumInput => {
+                if (officeNumInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an office number to continue!');
+                }
             }
-        }
-    }
-];
+        },
 
-// Function to init app
-function startApp() {
-    inquirer.prompt(managerQuestions)
-        .then(function (answers) {
-            writetoFile("index.html", generateHTML)
-        });
+    ])
+        .then(function (managerInput) {
+            const { name, id, email, officeNum } = managerInput;
+            const manager = new Manager(name, id, email, officeNum);
+
+            teamArr.push(manager);
+            console.log(teamArr);
+        })
+
 };
 
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "addTeam",
+            message: "What would you like to do next? Please CHOOSE 1",
+            choices: [
+                "Add Engineer",
+                "Add Intern",
+                new inquirer.Separator(),
+                "Finish Building My Team "
+            ]
+
+        }
+
+
+    ])
+};
+
+
 // start app 
-startApp();
+managerQuestions()
+    .then(addEmployee);
+
